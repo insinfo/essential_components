c1595e0c34098feb6911063560266bfafa197e91
insinfo <insinfo2008@gmail.com>
2019-08-12 11:32:38 -0300

    initial commit

---
+++ example/essential_components_example.dart
@@ -0,0 +1,6 @@
+import 'package:essential_components/essential_components.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/essential_components.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library essential_components;
+
+export 'src/essential_components_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
