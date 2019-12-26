a5edbd10707a6cc35c68d840bab4a394ac5c7a51
Thiago <thiagosantoscunha@outlook.com>
2019-12-26 17:48:06 -0300

    Manu de navegação de molde de conteúdos

--- example/lib/app_component.dart
+++ example/lib/app_component.dart
@@ -3,12 +3,9 @@ import 'package:angular_forms/angular_forms.dart';
 import 'package:angular_router/angular_router.dart';
 
 //components
-import 'package:essential_components/essential_components.dart';
 
-//models
-import 'src/models/user.dart';
-
-import 'dart:html' as html;
+import 'package:example/src/components/menu_sidebar/menu_aside_component/menu_aside_component.dart';
+import 'package:example/src/components/menu_sidebar/menu_sidebar_options_component/menu_sidebar_options_component.dart';
 
 @Component(
     selector: 'my-app',
@@ -18,6 +15,8 @@ import 'dart:html' as html;
       formDirectives,
       coreDirectives,
       routerDirectives,
+      MenuAsideComponent,
+      MenuSidebarOptionsComponent
     ],
     exports: [])
 class AppComponent {
---
+++ example/lib/src/components/menu_sidebar/menu_sidebar_options_component/menu_sidebar_options_component.dart
@@ -0,0 +1,20 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'menu-sidebar-options',
+  styleUrls: ['menu_sidebar_options_component.css'],
+  templateUrl: 'menu_sidebar_options_component.html',
+  directives: [
+    coreDirectives,
+  ],
+  exports: []
+)
+class MenuSidebarOptionsComponent implements OnInit {
+
+  MenuSidebarOptionsComponent();
+
+  @override
+  void ngOnInit() async {
+  }
+
+}
